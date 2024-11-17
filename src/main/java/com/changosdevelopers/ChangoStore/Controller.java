61050f6c1c362f9d13c02368713dcc96f939076a
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-11-16 20:14:44 -0800

    products list

--- src/main/java/com/changosdevelopers/ChangoStore/Controller.java
+++ src/main/java/com/changosdevelopers/ChangoStore/Controller.java
@@ -11,16 +11,19 @@ import java.util.List;
 public class Controller {
 
     @GetMapping("/Articles")
-    public String home(){
-        return "pages/ArticlesPage";
-    }
-
-    @GetMapping("/articulos")
     public String getArticules(Model model){
         List<Articles> articulos = new ArrayList<>();
         articulos.add(new Articles("Laptop",999));
-        articulos.add(new Articles("La",123));
-        articulos.add(new Articles("LaAWDAWd",1123));
+        articulos.add(new Articles("Tablet",123));
+        articulos.add(new Articles("Celular",1123));
+        articulos.add(new Articles("Iphone",1123));
+        articulos.add(new Articles("Pc",1123));
+        articulos.add(new Articles("Pescado",1123));
+        articulos.add(new Articles("Pollo",1123));
+        articulos.add(new Articles("Moto",1123));
+        articulos.add(new Articles("UABC",1123));
+        articulos.add(new Articles("Escuela primaria secundaria tecinca numero 23",1123));
+        articulos.add(new Articles("Hola",1123));
 
         model.addAttribute("lista",articulos);
 
