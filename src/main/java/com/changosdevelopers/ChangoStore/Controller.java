26c79eb7354fb27a175839a91b15b96837645347
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-11-20 16:13:19 -0800

    Search, add y delete complete

--- src/main/java/com/changosdevelopers/ChangoStore/Controller.java
+++ src/main/java/com/changosdevelopers/ChangoStore/Controller.java
@@ -4,7 +4,9 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +25,7 @@ public class Controller {
     public  String showArticles(Model mod){
         try {
             // Traer archivos
-            List<Articles> articulos = jsonConnect.getJsonArticules();
+            List<Articles> articulos = jsonConnect.getArticles();
             mod.addAttribute("listaArticulos",articulos);
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,6 +34,21 @@ public class Controller {
         return "pages/ArticlesPage";
     }
 
+    @GetMapping("/search/{name}")
+    public String searchArticle(@PathVariable String name, Model model) throws IOException {
+        Articles article = jsonConnect.searchArticle(name);
+        if(article == null){
+            model.addAttribute("articulo",new Articles("error",9.0));
+        }else{
+            model.addAttribute("articulo",article);
+        }
+        double pr = article.getPrice();
+
+        String url = "/article/" + article.getName() + "/" + pr;
+
+        // Redirigir a la URL deseada con los parámetros
+        return "redirect:"+url;
+    }
 
     @GetMapping("/article/{name}/{price}")
     public String articleInfo(@PathVariable String name,@PathVariable double price,Model model){
@@ -39,4 +56,29 @@ public class Controller {
         model.addAttribute("articulo",artSelected);
         return "pages/ArticleInfo";
     }
+
+    @GetMapping("/add/{name}/{price}")
+    public String addArticle(@PathVariable String name, @PathVariable double price, Model model)throws IOException {
+        Articles temp = new Articles(name,price);
+        jsonConnect.addValue(temp);
+        return "pages/done";
+    }
+
+    @GetMapping("/delete/{name}")
+    public String deleteArticle(@PathVariable String name, Model model)throws IOException {
+        jsonConnect.deleteValue(name);
+        return "pages/done";
+    }
+
+    @GetMapping("/car")
+    public String shopCar(Model model){
+        try {
+            List<Articles> lista = jsonConnect.getShopCar();
+            model.addAttribute("listaArticulos",lista);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "pages/car";
+    }
+
 }
--- src/main/java/com/changosdevelopers/ChangoStore/jsonService.java
+++ src/main/java/com/changosdevelopers/ChangoStore/jsonService.java
@@ -7,16 +7,68 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class jsonService {
+    // Json - > Java && Java - > Json
     private final ObjectMapper obj = new ObjectMapper();
 
-    public List<Articles> getJsonArticules() throws IOException {
-        //Cargar el archivo desde resources
-        File json = new File(getClass().getClassLoader().getResource("articles.json").getFile());
+    private final String dataBase = "src/main/resources/articles.json";
+    private final String shopCar = "src/main/resources/carrito.json";
 
-        // Lee el archivo y lo convierte en una lista de objetos articulos
-        return obj.readValue(json, new TypeReference<List<Articles>>() {});
+
+    public void saveJson(List<Articles> articles) throws IOException{
+        obj.writerWithDefaultPrettyPrinter().writeValue(new File(dataBase),articles);
+    }
+
+    public void saveShopCar(List<Articles> articles) throws IOException{
+        obj.writerWithDefaultPrettyPrinter().writeValue(new File(shopCar),articles);
+    }
+
+    public List<Articles> getArticles() throws IOException{
+        File bd = new File(dataBase);
+        if(!bd.exists()){
+            return new ArrayList<>();
+        }
+        return obj.readValue(bd, new TypeReference<List<Articles>>() {});
+    }
+
+    public List<Articles> getShopCar()throws IOException{
+        File car = new File(shopCar);
+        if(!car.exists()){
+            return new ArrayList<>();
+        }
+        return obj.readValue(car, new TypeReference<List<Articles>>() {});
+    }
+
+    public void addValue(Articles newArticle) throws IOException {
+        List<Articles> articlesList = getShopCar();
+        articlesList.add(newArticle);
+        saveShopCar(articlesList);
+    }
+
+    public boolean deleteValue(String name) throws  IOException{
+        List<Articles> articlesList = getShopCar();
+        boolean done = false;
+        for( Articles ar : articlesList){
+          if(ar.getName().equals(name)){
+              articlesList.remove(ar);
+              saveShopCar(articlesList);
+              done=true;
+              break;
+          }
+        }
+        return done;
+    }
+
+    public Articles searchArticle(String name) throws IOException {
+        List<Articles> list = getArticles();
+        for(Articles temp : list){
+            if(temp.getName().equals(name)){
+                return temp;
+            }
+        }
+        return new Articles("ADAA",20);
     }
 }
